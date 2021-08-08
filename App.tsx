a60beed60e503604142a987d7865f38d1ecdf7da
Salman Shaikh <tony903212@gmail.com>
2021-08-08 16:51:18 +0530

    Basic weather by search using metaweather API

--- App.tsx
+++ App.tsx
@@ -8,28 +8,130 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
+	Image,
 	SafeAreaView,
 	ScrollView,
 	StatusBar,
 	StyleSheet,
 	Text,
+	TextInput,
 	useColorScheme,
 	View,
 } from 'react-native';
 
+interface ConsolidatedWeather {
+	air_pressure: number;
+	applicable_date: string;
+	created: string;
+	humidity: number;
+	id: number;
+	max_temp: number;
+	min_temp: number;
+	predictability: number;
+	the_temp: number;
+	visibility: number;
+	weather_state_abbr: string;
+	weather_state_name: string;
+	wind_direction: number;
+	wind_direction_compass: string;
+	wind_speed: number;
+}
+
+interface Weather {
+	consolidated_weather: ConsolidatedWeather[];
+}
+
 const App = () => {
 	const isDarkMode = useColorScheme() === 'dark';
+	const [location, setLocation] = useState('');
+	const [weather, setWeather] = useState<Weather>();
+
+	const searchCity = async () => {
+		const url =
+			'https://www.metaweather.com/api/location/search/?query=' +
+			location;
+		const response = await fetch(url);
+		const data = await response.json();
+		console.log(data);
+		const weatherUrl = `https://www.metaweather.com/api/location/${data[0].woeid}`;
+		const weatherResponse = await fetch(weatherUrl);
+		const weatherData = await weatherResponse.json();
+		setWeather(weatherData);
+	};
 
 	return (
 		<SafeAreaView>
 			<StatusBar
 				barStyle={isDarkMode ? 'light-content' : 'dark-content'}
 			/>
+			<TextInput
+				style={styles.searchBar}
+				onChangeText={setLocation}
+				onSubmitEditing={searchCity}
+				placeholder="Search City"
+				returnKeyType="search"
+			/>
+
 			<ScrollView contentInsetAdjustmentBehavior="automatic">
 				<View>
-					<Text style={styles.text}>Hello World</Text>
+					{weather ? (
+						<View style={styles.weatherCard}>
+							<Image
+								source={{
+									uri: `https://www.metaweather.com/static/img/weather/png/${weather.consolidated_weather[0].weather_state_abbr}.png`,
+									height: 64,
+									width: 64,
+								}}
+							/>
+							<Text>{location}</Text>
+							<Text>
+								{weather.consolidated_weather[0].the_temp.toFixed()}
+								&deg; C
+							</Text>
+							<View>
+								<Text>
+									Min:{' '}
+									{weather.consolidated_weather[0].min_temp.toFixed()}
+									&deg; C
+								</Text>
+								<Text>
+									Max:{' '}
+									{weather.consolidated_weather[0].max_temp.toFixed()}
+									&deg; C
+								</Text>
+							</View>
+						</View>
+					) : null}
+					{weather
+						? weather.consolidated_weather.map(entry => (
+								<View key={entry.id} style={styles.weatherCard}>
+									<Image
+										source={{
+											uri: `https://www.metaweather.com/static/img/weather/png/${entry.weather_state_abbr}.png`,
+											height: 64,
+											width: 64,
+										}}
+									/>
+									<Text>
+										{entry.the_temp.toFixed()}
+										&deg; C
+									</Text>
+									<View>
+										<Text>
+											Min: {entry.min_temp.toFixed()}
+											&deg; C
+										</Text>
+										<Text>
+											Max: {entry.max_temp.toFixed()}
+											&deg; C
+										</Text>
+									</View>
+								</View>
+								// eslint-disable-next-line no-mixed-spaces-and-tabs
+						  ))
+						: null}
 				</View>
 			</ScrollView>
 		</SafeAreaView>
@@ -37,26 +139,20 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-	text: {
-		padding: 20,
-		textAlign: 'center',
-		fontSize: 21,
+	searchBar: {
+		fontSize: 16,
+		padding: 15,
+		shadowColor: 'black',
+		shadowOpacity: 100,
+		shadowOffset: {width: 15, height: 5},
+		shadowRadius: 50,
 	},
-	sectionContainer: {
-		marginTop: 32,
-		paddingHorizontal: 24,
-	},
-	sectionTitle: {
-		fontSize: 24,
-		fontWeight: '600',
-	},
-	sectionDescription: {
-		marginTop: 8,
-		fontSize: 18,
-		fontWeight: '400',
-	},
-	highlight: {
-		fontWeight: '700',
+	weatherCard: {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		padding: 25,
+		justifyContent: 'space-between',
 	},
 });
 
